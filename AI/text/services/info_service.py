577b4f857f6bf68ca45d99d6018549606ccd95ab
오주현 <ojh1105@naver.com>
2024-05-24 13:47:57 +0900

    feat: 멀티 프로세싱 예외 처리
    Change-Id: I650b3178e24bf5dedb49c05c8630ddf1712f46b6

--- AI/text/services/info_service.py
+++ AI/text/services/info_service.py
@@ -4,6 +4,7 @@ from internals.text_analyzer import infoDetector
 class InfoService:
     def detail_info_detection(self, texts: tuple[list]) -> tuple[list, list]:
         texts = texts[0]
+        print(texts)
         return infoDetector.detect_texts(texts)
 
 
--- AI/text/services/process_executor.py
+++ AI/text/services/process_executor.py
@@ -9,5 +9,8 @@ executor = ProcessPoolExecutor(max_workers=settings.max_workers)
 async def process_executor(task, *args):
     loop = asyncio.get_running_loop()
     print(task)
-    result = await loop.run_in_executor(executor, task, args)
-    return result
+    try:
+        result = await loop.run_in_executor(executor, task, args)
+        return result
+    except Exception as e:
+        print(f"Error in run_in_executor: {e}")
