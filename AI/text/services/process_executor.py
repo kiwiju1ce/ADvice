77344509bfe1ec97f65d5477803fd53a9e4868d9
오주현 <ojh1105@naver.com>
2024-05-24 15:37:55 +0900

    feat: set debug mode to track error
    Change-Id: I64504e278a0b18d22af70709cfa7f81cb2115be4

--- AI/text/services/process_executor.py
+++ AI/text/services/process_executor.py
@@ -7,7 +7,17 @@ executor = ProcessPoolExecutor(max_workers=settings.max_workers)
 
 
 async def process_executor(task, *args):
+    # future = executor.submit(task, args)
+    # try:
+    #     result = asyncio.wrap_future(future)
+    #     print(f"Result: {result}")
+    # except asyncio.TimeoutError:
+    #     print("Task timed out")
+    # except Exception as e:
+    #     print(f"Error in executor.submit: {e}")
     loop = asyncio.get_running_loop()
+    print(loop.is_running())
+    loop.set_debug(True)
     print(task)
     try:
         result = await loop.run_in_executor(executor, task, args)
